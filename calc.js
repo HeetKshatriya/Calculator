918837c3a0a1170852888469aea6baaa3a7a84bd
HeetKshatriya <heetkshatriya1645@gmail.com>
2025-03-29 21:24:19 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,17 @@
+function appendNumber(num) {
+    const display = document.getElementById('display');
+    display.value = display.value + num;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
